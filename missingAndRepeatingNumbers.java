0b66e72976ecfaf771a5e7886aa6c71ebdbc949c
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2022-06-14 20:30:34 +0530

    Day 4 : all commits

---
+++ missingAndRepeatingNumbers.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class missingAndRepeatingNumbers {
+
+    public static int[] missingAndRepeating(ArrayList<Integer> arr, int n) {
+
+        int r = 0, m = 0;
+
+        // Traversing the array
+        for (int i = 0; i < n; i += 1) {
+            if (arr.get(Math.abs(arr.get(i)) - 1) > 0) {
+
+                int x = Math.abs(arr.get(i));
+
+                arr.set(x - 1, -arr.get(x - 1));
+            }
+            // If arr[i] is already negative, it is the repeating number
+            else {
+                r = Math.abs(arr.get(i));
+            }
+        }
+
+        for (int i = 0; i < n; i += 1) {
+            // The index at which the element is positive is the missing number
+            if (arr.get(i) > 0) {
+                m = i + 1;
+                break;
+            }
+        }
+
+        int[] ans = new int[]{m, r};
+
+        return ans;
+    }
+}
+
